a83181771270f325ea3f4b200a6ee117930fc618
majora2004 <marcos.ferblaz@proton.me>
2025-01-30 13:49:21 +0100

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,12 +6,24 @@ root.title("Mi Primera App")  # Título de la ventana
 root.geometry("400x300")      # Tamaño de la ventana (ancho x alto)
 
 # Aquí agregarás widgets (botones, etiquetas, etc.)
-label=tk.Label(root, text="hola caracola")
+label=tk.Label(root, text="youtube download tool")
 label.pack()  # Agrega la etiqueta a la ventana
 
+label=tk.Label(root, text="¿quiere decargar el video o solo el audio?")
+label.pack()
 
-def on_button_click():
-    print("arriba españa")
+radio_var = tk.StringVar(value="1")
 
+radio1 = tk.Radiobutton(root, text="video", variable=radio_var, value="1")
+radio1.pack()
+
+radio2= tk.Radiobutton(root, text="audio", variable=radio_var, value="2")
+radio2.pack()
+
+def get_radio():
+    print("Opción seleccionada:", radio_var.get())
+
+button = tk.Button(root, text="Obtener descarga", command=get_radio)
+button.pack()
 # Iniciar el bucle principal de la aplicación
 root.mainloop()
